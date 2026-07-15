89bef78e4cde034472b17522f9f22eae1a010093
aidankhogg <aidankhogg@gmail.com>
2026-07-15 10:17:10 +0100

    Clean up compute architecture API

--- builds/packages/base/internetworking/src/computecommons/compute/__init__.py
+++ builds/packages/base/internetworking/src/computecommons/compute/__init__.py
@@ -1,5 +1,17 @@
+from computecommons.compute.architecture import (
+    ARCHITECTURE_ALIASES,
+    CPUArchitecture,
+    normalize_architecture,
+)
 from computecommons.compute.cpu import CPUInfo
 from computecommons.compute.machine import Machine
 from computecommons.compute.memory import MemoryInfo
 
-__all__ = ["CPUInfo", "Machine", "MemoryInfo"]
+__all__ = [
+    "ARCHITECTURE_ALIASES",
+    "CPUArchitecture",
+    "CPUInfo",
+    "Machine",
+    "MemoryInfo",
+    "normalize_architecture",
+]
